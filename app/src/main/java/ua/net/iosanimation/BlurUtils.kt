bb8509897aa1bd53a97300ac2e0faf106679add7
Evgeny Bozhko <smail2133@gmail.com>
2022-01-25 12:23:34 +0200

    Add blur background.

---
+++ app/src/main/java/ua/net/iosanimation/BlurUtils.kt
@@ -0,0 +1,45 @@
+package ua.net.iosanimation
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.renderscript.Allocation
+import androidx.renderscript.Element
+import androidx.renderscript.RenderScript
+import androidx.renderscript.ScriptIntrinsicBlur
+import kotlin.math.roundToInt
+
+
+object BlurUtils {
+
+    private const val BITMAP_SCALE = 0.3f
+    private const val BLUR_RADIUS = 25f
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    fun renderEffect(): RenderEffect =
+        RenderEffect.createBlurEffect(BLUR_RADIUS, BLUR_RADIUS, Shader.TileMode.REPEAT)
+
+    fun blur(context: Context, image: Bitmap): Bitmap {
+        val width = (image.width * BITMAP_SCALE).roundToInt()
+        val height = (image.height * BITMAP_SCALE).roundToInt()
+
+        val inputBitmap = Bitmap.createScaledBitmap(image, width, height, false)
+        val outputBitmap = Bitmap.createBitmap(inputBitmap)
+
+        val rs: RenderScript = RenderScript.create(context)
+        val intrinsicBlur: ScriptIntrinsicBlur = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs))
+
+        val tmpIn: Allocation = Allocation.createFromBitmap(rs, inputBitmap)
+        val tmpOut: Allocation = Allocation.createFromBitmap(rs, outputBitmap)
+
+        intrinsicBlur.setRadius(BLUR_RADIUS)
+        intrinsicBlur.setInput(tmpIn)
+        intrinsicBlur.forEach(tmpOut)
+        tmpOut.copyTo(outputBitmap)
+
+        return outputBitmap
+    }
+}
---
+++ app/src/main/java/ua/net/iosanimation/MainActivityViewModel.kt
@@ -0,0 +1,32 @@
+package ua.net.iosanimation
+
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+
+class MainActivityViewModel : ViewModel() {
+
+    fun blurBitmapWithRenderScript(@DrawableRes image: Int, context: Context): LiveData<Bitmap> {
+        val lv = MutableLiveData<Bitmap>()
+
+        viewModelScope.launch(Dispatchers.IO) {
+            val bm = ContextCompat.getDrawable(context, image)?.toBitmap()
+
+            bm?.let {
+               val blurBitmap = BlurUtils.blur(context = context, image = it)
+                lv.postValue(blurBitmap)
+            }
+        }
+
+        return lv
+    }
+}
