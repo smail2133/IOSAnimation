1ac8ea374b96805b4a037af8f50811dd21a0f2b0
Evgeny Bozhko <smail2133@gmail.com>
2022-01-28 13:10:14 +0200

    Migrate animation to single container.

--- app/src/main/java/ua/net/iosanimation/activity/MainActivity.kt
+++ app/src/main/java/ua/net/iosanimation/activity/MainActivity.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import ua.net.iosanimation.*
+import ua.net.iosanimation.R
 import ua.net.iosanimation.databinding.ActivityMainBinding
 import ua.net.iosanimation.utils.*
 
@@ -26,50 +26,36 @@ class MainActivity : AppCompatActivity() {
         bnd.run {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                 val blurEffect = BlurUtils.renderEffect()
-                bnd.backgroundBlur.setImageResource(R.drawable.backgound_ios)
+                bnd.backgroundBlur.setImageResource(R.drawable.background_ios)
                 bnd.backgroundBlur.setRenderEffect(blurEffect)
             } else {
                 vm.blurBitmapWithRenderScript(
-                    image = R.drawable.backgound_ios,
+                    image = R.drawable.background_ios,
                     context = this@MainActivity
                 )
-                    .observe(this@MainActivity, {
+                    .observe(this@MainActivity) {
                         bnd.backgroundBlur.setImageBitmap(it)
-                    })
+                    }
             }
         }
 
         setContentView(bnd.root)
 
-        bnd.smallMenu.setOnLongClickListener {
+        bnd.menu.setOnLongClickListener {
             it.playHapticFeedback()
-            expandMenu()
+            toggleMenu()
             true
         }
-        bnd.smallMenu.setDebounceListener {
-            expandMenu()
-        }
-        bnd.extendedMenu.setDebounceListener {
-            collapseMenu()
+        bnd.menu.setDebounceListener {
+            toggleMenu()
         }
 
         bnd.button1.setOnClickListener(toastClick)
         bnd.button2.setOnClickListener(toastClick)
         bnd.button3.setOnClickListener(toastClick)
         bnd.button4.setOnClickListener(toastClick)
-        bnd.buttonExtended1.setOnClickListener(toastClick)
-        bnd.buttonExtended2.setOnClickListener(toastClick)
-        bnd.buttonExtended3.setOnClickListener(toastClick)
-        bnd.buttonExtended4.setOnClickListener(toastClick)
-        bnd.buttonExtended5.setOnClickListener(toastClick)
-        bnd.buttonExtended6.setOnClickListener(toastClick)
-
-        bnd.buttonExtended1.setOnLongClickListener(bluetoothLongClick)
-        bnd.buttonExtended2.setOnLongClickListener(bluetoothLongClick)
-        bnd.buttonExtended3.setOnLongClickListener(bluetoothLongClick)
-        bnd.buttonExtended4.setOnLongClickListener(bluetoothLongClick)
-        bnd.buttonExtended5.setOnLongClickListener(bluetoothLongClick)
-        bnd.buttonExtended6.setOnLongClickListener(bluetoothLongClick)
+        bnd.button5.setOnClickListener(toastClick)
+        bnd.button6.setOnClickListener(toastClick)
     }
 
     private val toastClick = View.OnClickListener { p0 ->
@@ -82,75 +68,47 @@ class MainActivity : AppCompatActivity() {
         ).show()
     }
 
-    private val bluetoothLongClick = View.OnLongClickListener {
-        it.iosPulse {
-            it.playHapticFeedback()
-            expandBluetoothMenu()
-        }
-        true
-    }
-
     override fun onBackPressed() {
-        if (bnd.extendedMenu.visibility == View.VISIBLE) {
+        if (bnd.button5.visibility == View.VISIBLE) {
             collapseMenu()
-        } else if (bnd.bluetoothMenu.visibility == View.VISIBLE) {
-            collapseBluetoothMenu()
         } else {
             super.onBackPressed()
         }
     }
 
+    private fun toggleMenu() {
+        if (bnd.button5.visibility == View.VISIBLE) {
+            collapseMenu()
+        } else {
+            expandMenu()
+        }
+    }
+
     private fun expandMenu() {
-        bnd.smallMenu.playHapticFeedback()
+        bnd.menu.playHapticFeedback()
 
-        bnd.smallMenu.iosPulse {
-            expandCollapseAnimation(
-                fromV = bnd.smallMenu,
-                toV = bnd.extendedMenu,
-                container = bnd.root,
-                isExpand = true
-            )
-
+        bnd.menu.iosPulse {
             //change view scale to begin while it invisible.
-            bnd.smallMenu.scaleX = 1f
-            bnd.smallMenu.scaleY = 1f
+            bnd.menu.scaleX = 1f
+            bnd.menu.scaleY = 1f
+
+            expandCollapseSingleContainerAnimation(container = bnd.menu, isExpand = true)
+            visibilityChangeAnimation(
+                container = bnd.menu,
+                views = arrayOf(bnd.button5, bnd.button6),
+                visibility = View.VISIBLE
+            )
         }
     }
 
     private fun collapseMenu() {
-        bnd.extendedMenu.playHapticFeedback()
+        bnd.menu.playHapticFeedback()
 
-        expandCollapseAnimation(
-            fromV = bnd.smallMenu,
-            toV = bnd.extendedMenu,
-            container = bnd.root,
-            isExpand = false
+        expandCollapseSingleContainerAnimation(container = bnd.menu, isExpand = false)
+        visibilityChangeAnimation(
+            container = bnd.menu,
+            views = arrayOf(bnd.button5, bnd.button6),
+            visibility = View.GONE
         )
-    }
-
-    private fun expandBluetoothMenu() {
-        expandCollapseAnimation(
-            fromV = bnd.extendedMenu,
-            toV = bnd.bluetoothMenu,
-            container = bnd.root,
-            isExpand = true
-        )
-
-        bnd.bluetoothMenu.iosPulse {
-            bnd.bluetoothMenu.iosPulseBack { }
-        }
-    }
-
-    private fun collapseBluetoothMenu() {
-        expandCollapseAnimation(
-            fromV = bnd.extendedMenu,
-            toV = bnd.bluetoothMenu,
-            container = bnd.root,
-            isExpand = false
-        )
-
-        bnd.extendedMenu.iosPulse {
-            bnd.extendedMenu.iosPulseBack { }
-        }
     }
 }
--- app/src/main/java/ua/net/iosanimation/utils/AnimationUtils.kt
+++ app/src/main/java/ua/net/iosanimation/utils/AnimationUtils.kt
@@ -3,10 +3,13 @@ package ua.net.iosanimation.utils
 import android.animation.Animator
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
+import android.animation.ValueAnimator
 import android.graphics.Color
 import android.view.View
 import android.view.ViewGroup
 import androidx.interpolator.view.animation.FastOutSlowInInterpolator
+import androidx.transition.Fade
+import androidx.transition.Transition
 import androidx.transition.TransitionManager
 import com.google.android.material.transition.MaterialArcMotion
 import com.google.android.material.transition.MaterialContainerTransform
@@ -105,4 +108,58 @@ fun expandCollapseAnimation(fromV: View, toV: View, container: ViewGroup, isExpa
     }
 
     TransitionManager.beginDelayedTransition(container, transform)
+}
+
+fun expandCollapseSingleContainerAnimation(container: ViewGroup, isExpand: Boolean) {
+    val heightAnimator =
+        ValueAnimator.ofInt(
+            container.measuredHeight,
+            if (isExpand) container.measuredHeight * 2 else container.measuredHeight / 2
+        )
+    heightAnimator.duration =
+        container.resources.getInteger(R.integer.transform_animation_duration).toLong()
+    heightAnimator.addUpdateListener {
+        val animatedValue = heightAnimator.animatedValue as Int
+        val layoutParams = container.layoutParams
+        layoutParams.height = animatedValue
+
+        container.requestLayout()
+    }
+
+    val widthAnimator =
+        ValueAnimator.ofInt(
+            container.measuredWidth,
+            if (isExpand) container.measuredWidth * 2 else container.measuredWidth / 2
+        )
+    widthAnimator.duration =
+        container.resources.getInteger(R.integer.transform_animation_duration).toLong()
+    widthAnimator.addUpdateListener {
+        val animatedValue = widthAnimator.animatedValue as Int
+        val layoutParams = container.layoutParams
+        layoutParams.width = animatedValue
+
+        container.requestLayout()
+    }
+
+    heightAnimator.start()
+    widthAnimator.start()
+}
+
+fun visibilityChangeAnimation(container: ViewGroup, vararg views: View, visibility: Int) {
+    val transition: Transition = Fade()
+
+    transition.apply {
+        interpolator = FastOutSlowInInterpolator()
+        duration = container.resources.getInteger(R.integer.transform_animation_duration).toLong()
+    }
+
+    views.map {
+        transition.addTarget(container)
+    }
+
+    TransitionManager.beginDelayedTransition(container, transition)
+
+    views.map {
+        it.visibility = visibility
+    }
 }
